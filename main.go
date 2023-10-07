ce339fc32278650bf8cc58e15ad70d3440a2d2f5
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2023-10-07 11:21:22 -0300

    inico y confirguración de proyecto del curso bootdev

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	godotenv.Load(".env") // Con esto traemos las variables del archivo .env al ambiente en el que nos encontramos
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the enviroment")
+	}
+
+	router := chi.NewRouter() // Creamos un nuevo router
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	// Configuramos el servidor
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	//Damos de alta el servidor. En caso de haber error será informado mediante el log.Fatal(err)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Port:", portString)
+}
